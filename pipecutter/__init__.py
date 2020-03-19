84a0adebb208d077085bb0ba84dbe7f3e4ed22a1
binste <binder.stefan@hispeed.ch>
2020-03-19 13:42:47 +0100

    Add dev to version

--- pipecutter/__init__.py
+++ pipecutter/__init__.py
@@ -6,4 +6,4 @@ from pipecutter.deps import print_tree, build_graph
 from pipecutter.interface import run
 from pipecutter.targets import remove_targets
 
-__version__ = "2.0.0"
+__version__ = "2.1.0dev"
